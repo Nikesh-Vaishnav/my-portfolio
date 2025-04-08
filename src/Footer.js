2785d5606af8482006aa8c582d825f6d10b420cd
Nikesh vaishnav <nikeshvaishnav377@gmail.com>
2025-04-09 02:50:24 +0530

    update  a  contact link

--- src/Footer.js
+++ src/Footer.js
@@ -6,16 +6,16 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="social-icons">
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/_nikesh_vaishnav_/" target="_blank" rel="noopener noreferrer">
           <FaInstagram />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" target="_blank" rel="noopener noreferrer">
           <FaLinkedin />
         </a>
         <a href="https://t.me" target="_blank" rel="noopener noreferrer">
           <FaTelegram />
         </a>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/_nikesh_vaishnav_/" target="_blank" rel="noopener noreferrer">
           <FaGithub />
         </a>
       </div>
